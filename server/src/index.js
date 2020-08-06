afc3deb2e5c2b1655cc842b88898102d4963240f
ivan rubio <ivanmac@ivans-MacBook-Pro.local>
2020-08-06 13:34:47 -0500

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+const express =require("express");
+const app = express();
+const port = 9000;
+
+const one = 1;
+let two = 2;
+two = "two"
+
+app.get("/", (req, res) => res.send(`1 + 2 = ${ one + two }`));
+
+app.listen(port);
+
+console.log(`[app]: http/localhost${port}`);
